da922e8950756fd37a122a1a9cc3778b31f004dd
Glitch (glitch-hello-website) <none>
2023-07-30 03:35:10 +0000

    🐯🍰 Checkpoint ./main.js:56435016/486

--- main.js
+++ main.js
@@ -168,9 +168,13 @@ const App = {
         if(data) {
             fetch(apiUrl + "/love", {
                     method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
                     body: JSON.stringify(data)
                 })
-            .then(res => {r})
+            .then(res => res.json())
+            .then(data => console.log(data))
             .catch(err => {console.log(err);});
         }
     },
